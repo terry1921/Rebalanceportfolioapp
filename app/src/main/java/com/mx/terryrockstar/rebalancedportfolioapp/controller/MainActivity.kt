696f5512e41f239c25e0b70c5b1b6abb5201b091
Enrique Espinoza <terryrockstar22@gmail.com>
2022-04-17 08:21:01 -0500

    build: Use viewBinding

--- app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/controller/MainActivity.kt
+++ app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/controller/MainActivity.kt
@@ -3,11 +3,23 @@ package com.mx.terryrockstar.rebalancedportfolioapp.controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mx.terryrockstar.rebalancedportfolioapp.R
+import com.mx.terryrockstar.rebalancedportfolioapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_RebalancedPortfolioApp)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.linkPrivacyPolice.setOnClickListener {  }
+        binding.startButton.setOnClickListener {  }
+    }
+
 }
