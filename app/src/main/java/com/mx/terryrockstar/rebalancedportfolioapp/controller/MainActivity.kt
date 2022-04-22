cff97e8fe2f48b2c5b009dcb78b3c3ef19a0ceb7
Enrique Espinoza <terryrockstar22@gmail.com>
2022-04-21 21:02:58 -0500

    feat: Add Navigation Component from Android Jetpack

--- app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/controller/MainActivity.kt
+++ app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/controller/MainActivity.kt
@@ -1,26 +1,50 @@
 package com.mx.terryrockstar.rebalancedportfolioapp.controller
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.add
-import androidx.fragment.app.commit
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import com.mx.terryrockstar.rebalancedportfolioapp.R
+import com.mx.terryrockstar.rebalancedportfolioapp.utils.Print
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var appBarConfiguration : AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_RebalancedPortfolioApp)
         setContentView(R.layout.activity_main)
 
+        val toolbar = findViewById<Toolbar>(R.id.materialToolbar)
+        setSupportActionBar(toolbar)
 
-        if (savedInstanceState == null) {
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        //appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            val dest: String = try {
+                resources.getResourceName(destination.id)
+            } catch (e: Resources.NotFoundException) {
+                Print.e(javaClass.name, e.message, e.cause)
+                Integer.toString(destination.id)
+            }
+
+            Toast.makeText(this@MainActivity, "Navigated to $dest", Toast.LENGTH_SHORT).show()
+            Print.d("MainActivity", "Navigated to $dest")
+        }
+
+        /*if (savedInstanceState == null) {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<DisclaimerFragment>(R.id.fragment_container)
             }
-        }
+        }*/
 
     }
 
