cb8bce105049dc67b70cad0de91d28d3fcae9329
Enrique Espinoza <terryrockstar22@gmail.com>
2022-04-23 13:58:25 -0500

    feat: Add Fragments

--- app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/controller/MainActivity.kt
+++ app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/controller/MainActivity.kt
@@ -2,11 +2,16 @@ package com.mx.terryrockstar.rebalancedportfolioapp.controller
 
 import android.content.res.Resources
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.*
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mx.terryrockstar.rebalancedportfolioapp.R
 import com.mx.terryrockstar.rebalancedportfolioapp.utils.Print
 
@@ -16,7 +21,6 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setTheme(R.style.Theme_RebalancedPortfolioApp)
         setContentView(R.layout.activity_main)
 
         val toolbar = findViewById<Toolbar>(R.id.materialToolbar)
@@ -25,7 +29,11 @@ class MainActivity : AppCompatActivity() {
         val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment? ?: return
         val navController = host.navController
 
-        //appBarConfiguration = AppBarConfiguration(navController.graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        setupActionBar(navController, appBarConfiguration)
+
+        setupBottomNavMenu(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
             val dest: String = try {
@@ -39,13 +47,27 @@ class MainActivity : AppCompatActivity() {
             Print.d("MainActivity", "Navigated to $dest")
         }
 
-        /*if (savedInstanceState == null) {
-            supportFragmentManager.commit {
-                setReorderingAllowed(true)
-                add<DisclaimerFragment>(R.id.fragment_container)
-            }
-        }*/
+    }
 
+    private fun setupBottomNavMenu(navController: NavController) {
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+        bottomNav?.setupWithNavController(navController)
+    }
+
+    private fun setupActionBar(navController: NavController, appBarConfiguration: AppBarConfiguration) {
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController(R.id.fragment_container)) || super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.fragment_container).navigateUp(appBarConfiguration)
     }
 
 }
