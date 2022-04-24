cb63c6d88b020ab3d528ce06fc08b292728014a9
Enrique Espinoza <terryrockstar22@gmail.com>
2022-04-23 23:29:46 -0500

    feat: Update Add fragment

--- app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/add/AddFragment.kt
+++ app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/add/AddFragment.kt
@@ -1,4 +1,4 @@
-package com.mx.terryrockstar.rebalancedportfolioapp.controller
+package com.mx.terryrockstar.rebalancedportfolioapp.add
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
