9998d9990dc3ed219e9bb4e79d6f945472e3542c
Enrique Espinoza <terryrockstar22@gmail.com>
2022-04-23 23:29:03 -0500

    feat: update home fragment

---
+++ app/src/main/java/com/mx/terryrockstar/rebalancedportfolioapp/home/HomeFragment.kt
@@ -0,0 +1,53 @@
+package com.mx.terryrockstar.rebalancedportfolioapp.home
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import android.widget.TextView
+import android.widget.Toast
+import com.mx.terryrockstar.rebalancedportfolioapp.R
+import com.mx.terryrockstar.rebalancedportfolioapp.databinding.FragmentHomeBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * create an instance of this fragment.
+ */
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    // TODO only for view data model
+    // private val viewModel by viewModels<HomeViewModel> { getViewModelFactory() }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.bind(view).apply {
+            // viewmodel = viewModel
+        }
+        setHasOptionsMenu(true)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        binding.noData.titleNoData.text = getString(R.string.no_assets)
+        binding.noData.root.visibility = View.VISIBLE
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.add) {
+            Toast.makeText(context, "Add Asset", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
